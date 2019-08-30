70dc686fc539741f760acadb3da88f910ae1528a
TaoPanfeng <1801957499@qq.com>
2019-08-30 15:13:12 +0800

    第二次提交,初步设置顶部栏,设置了App.vue,使用了Mint UI

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app_container">
+        <!--顶部-->
+        <mt-header fixed title="固定在顶部"></mt-header>
+        <!--中间 路由 router-view-->
+
+        <!--底部-->
+        <h1 id="aaa">我是主文件_App.vue</h1>
+    </div>
+</template>
+
+<script></script>
+
+<!--lang表示设置样式为 scss ,scoped表示此样式只能当前模版使用-->
+<style lang="scss" scoped>
+    .app_container
+    {
+        padding-top: 40px; //让固定在顶部与<h1>标签内容不重合
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("测试");
+/*Vue*/
+import Vue from 'vue'
+/*Mint UI*/
+import MintUI from 'mint-ui'
+Vue.use(MintUI);
+import 'mint-ui/lib/style.css'
+/*App.vue*/
+import app from './App.vue'
+new Vue({
+    el: "#app",
+    render: c => c(app),
+});

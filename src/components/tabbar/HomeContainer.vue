2412201a5e947a2d7b2b24bc3ae72e9059bad033
TaoPanfeng <1801957499@qq.com>
2019-08-30 16:46:30 +0800

    7,给首页容器中设置轮播图,并在HomeContainer.vue中设置样式

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,12 +1,38 @@
 <template>
     <div>
+        <!--轮播图-->
+        <mt-swipe id="aaa" :auto="1000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+
         <h3>HomeContainer</h3>
     </div>
 </template>
 
 <script></script>
 
-<!--lang设置为scss样式,scoped设置自己组件可用-->
-<style lang="scss" scoped>
+<!--scoped设置自己组件可用-->
+<style scoped>
+    .mint-swipe /*设置轮播图*/
+    {
+        height: 200px; /*高度:200,不然不显示,默认为0了*/
+    }
+
+    .mint-swipe-item:nth-child(1)/*第一个轮播图*/
+    {
+        background-color: red;
+    }
+
+    .mint-swipe-item:nth-child(2)/*第二个轮播图*/
+    {
+        background-color: yellow;
+    }
+
+    .mint-swipe-item:nth-child(3)/*第三个轮播图*/
+    {
+        background-color: blue;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 /*App.vue*/
 import app from './App.vue'
+
 new Vue({
     el: "#app",
     render: c => c(app),

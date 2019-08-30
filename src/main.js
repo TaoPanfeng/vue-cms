b49608124e41568eb39ca2f792f618ad3c3c97be
TaoPanfeng <1801957499@qq.com>
2019-08-30 15:18:04 +0800

    设置了底部栏,进行MUI渲染

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import MintUI from 'mint-ui'
 Vue.use(MintUI);
 import 'mint-ui/lib/style.css'
+/*MUI*/
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 /*App.vue*/
 import app from './App.vue'
 new Vue({

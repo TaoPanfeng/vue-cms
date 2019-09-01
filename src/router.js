6d68d7e969fd01cdbffb4ee7fc143e5d4f70f22c
TaoPanfeng <1801957499@qq.com>
2019-09-01 10:38:11 +0800

    11,改造了,首页-六宫格_1 路由 创建 NewsList.vue

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import CartContainer from './components/tabbar/CartContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
 
 export default new VueRouter({
     //匹配设置路由规则
@@ -14,6 +15,7 @@ export default new VueRouter({
             {path: "/member", component: MemberContainer},
             {path: "/cart", component: CartContainer},
             {path: "/search", component: SearchContainer},
+            {path: "/home/newslist", component: NewsList},
         ],
     // 会覆盖叫 router-link-active 默认的类
     linkActiveClass: 'mui-active'

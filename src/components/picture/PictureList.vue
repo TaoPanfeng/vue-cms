b26be990db4ff7dc5ff895baa999c20f637a354e
TaoPanfeng <1801957499@qq.com>
2019-09-02 13:13:19 +0800

    18,设置了首页-图片分享 的顶部导航条以及滑动问题

--- src/components/picture/PictureList.vue
+++ src/components/picture/PictureList.vue
@@ -1,13 +1,60 @@
 <template>
     <div>
-        <h3>图片列表</h3>
+        <!--顶部滑动条-->
+        <div id="slider" class="mui-slider">
+            <div id="sliderSegmentedControl"
+                 class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
+                <div class="mui-scroll">
+                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
+                        推荐
+                    </a>
+                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
+                        热点
+                    </a>
+                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
+                        北京
+                    </a>
+                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
+                        社会
+                    </a>
+                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
+                        娱乐
+                    </a>
+                    <a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
+                        我的
+                    </a>
+                    <a class="mui-control-item" href="#item7mobile" data-wid="tab-top-subpage-7.html">
+                        乖乖
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import mui from '../../lib/mui/js/mui.min.js'
 
+    export default {
+        data()
+        {
+            return {}
+        },
+        methods: {},
+        mounted()//组件中内容被渲染好之后
+        {
+            /*需要在.babelrc 中配置"transform-remove-strict-mode"*/
+            mui(".mui-scroll-wrapper").scroll({
+                deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+            });
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-
+    /*解决滑动报错: [HMR] Update failed: Error: Manifest request to*/
+    *
+      {
+          touch-action: pan-y;
+      }
 </style>

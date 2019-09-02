cc702e2a8b371740ff477d48a54ba43c18ca64bc
TaoPanfeng <1801957499@qq.com>
2019-09-02 13:43:02 +0800

    19,对顶部导航条进行数据加载,实现点击高亮

--- src/components/picture/PictureList.vue
+++ src/components/picture/PictureList.vue
@@ -5,26 +5,10 @@
             <div id="sliderSegmentedControl"
                  class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
                 <div class="mui-scroll">
-                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-                        推荐
-                    </a>
-                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-                        热点
-                    </a>
-                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-                        北京
-                    </a>
-                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-                        社会
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        娱乐
-                    </a>
-                    <a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-                        我的
-                    </a>
-                    <a class="mui-control-item" href="#item7mobile" data-wid="tab-top-subpage-7.html">
-                        乖乖
+                    <!--<a class="mui-control-item mui-active"-->
+                    <a :class="['mui-control-item', index == 0 ? 'mui-active' : '']"
+                       v-for="(category,index) in category_list" :key="index">
+                        {{category.title}}
                     </a>
                 </div>
             </div>
@@ -34,13 +18,32 @@
 
 <script>
     import mui from '../../lib/mui/js/mui.min.js'
+    import {Toast} from "mint-ui";
 
     export default {
         data()
         {
-            return {}
+            return {
+                category_list: []
+            }
         },
-        methods: {},
+        created()
+        {
+            this.get_all_category();
+        },
+        methods: {
+            get_all_category()
+            {
+                this.$http.get("/api/getimgcategory").then(result =>
+                {
+                    result.data.message.unshift({title: "全部", id: 0});
+                    this.category_list = result.data.message;
+                }).catch(() =>
+                {
+                    Toast("获取所有分类数据失败...");
+                });
+            }
+        },
         mounted()//组件中内容被渲染好之后
         {
             /*需要在.babelrc 中配置"transform-remove-strict-mode"*/
@@ -54,7 +57,7 @@
 <style lang="scss" scoped>
     /*解决滑动报错: [HMR] Update failed: Error: Manifest request to*/
     *
-      {
-          touch-action: pan-y;
-      }
+    {
+        touch-action: pan-y;
+    }
 </style>

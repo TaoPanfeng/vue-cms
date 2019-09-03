49c94a307cc018409f049eeae5178a4f7c6538e6
TaoPanfeng <1801957499@qq.com>
2019-09-03 08:04:31 +0800

    22,添加了GoodsList.vue 并添加路由,

---
+++ src/components/goods/GoodsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div goods_list_container>
+        <h3>我是商品列表</h3>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .goods_list_container
+    {
+
+    }
+</style>
